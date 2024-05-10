719e2dc9dc79a018c51bd3f3956c68549a2fdb62
Shashank <113260615+shashankaz@users.noreply.github.com>
2024-05-10 21:28:41 +0530

    minor fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,10 +12,10 @@ const Hero = () => {
         Coming Soon
       </h1>
       <div className="flex flex-col items-center justify-center">
-        <h3 className="md:text-2xl text-l">Get notified when it's ready</h3>
+        <h3 className="md:text-2xl text-lg">Get notified when it's ready</h3>
         <div className="flex items-center justify-center bg-[#444] md:w-[450px] w-[300px] md:h-[42px] h-[32px] rounded-[50px] mt-[10px]">
           <input
-            className="md:w-[300px] w-[220px] mx-6 border-none outline-none bg-[#444] text-white md:text-[15px] text-[11px]"
+            className="md:w-[300px] w-[220px] mx-6 border-none outline-none bg-[#444] text-white md:text-[15px] text-[13px]"
             type="email"
             placeholder="ENTER YOUR EMAIL"
             value={email}
@@ -24,7 +24,7 @@ const Hero = () => {
           />
           <button
             type="submit"
-            className="md:w-[150px] w-[130px] uppercase bg-black text-white h-full rounded-[50px] md:text-[15px] text-[11px]"
+            className="md:w-[150px] w-[140px] uppercase bg-black text-white h-full rounded-[50px] md:text-[15px] text-[13px]"
             onClick={submit}
           >
             Notify me
