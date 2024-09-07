eedda38ea06154a674a7aef4d26107a9334b6ad3
Shashank <113260615+shashankaz@users.noreply.github.com>
2024-09-07 23:37:58 +0530

    Refactor Hero component: Add new sliders for featured products

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,6 +26,27 @@ const Hero = () => {
         "https://images.pexels.com/photos/28155857/pexels-photo-28155857/free-photo-of-a-woman-in-a-pink-dress-sitting-on-a-chair.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
       }
     />,
+    <HeroSlider
+      key={3}
+      title="Be the First to Shop New Arrivals"
+      src={
+        "https://images.pexels.com/photos/19797382/pexels-photo-19797382/free-photo-of-beautiful-model-in-oregon-wearing-a-forest-green-linen-dress-portrait-taken-by-portland-photographer-lance-reis-on-my-sonya7iii-on-location.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      }
+    />,
+    <HeroSlider
+      key={4}
+      title="Elevate Your Style with Us"
+      src={
+        "https://images.pexels.com/photos/27782341/pexels-photo-27782341/free-photo-of-https-www-thesungwoo-com-new-page-93.jpeg?auto=compress&cs=tinysrgb&w=800"
+      }
+    />,
+    <HeroSlider
+      key={5}
+      title="Discover What's Trending"
+      src={
+        "https://images.pexels.com/photos/9736876/pexels-photo-9736876.jpeg?auto=compress&cs=tinysrgb&w=800"
+      }
+    />,
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
