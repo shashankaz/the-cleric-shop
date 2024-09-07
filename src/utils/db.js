72f7846a15f1a1bedb7739088ec11ba89c3271d1
Shashank <113260615+shashankaz@users.noreply.github.com>
2024-09-07 19:19:22 +0530

    Refactor database connection configuration

--- src/utils/db.js
+++ src/utils/db.js
@@ -13,8 +13,6 @@ export const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "the_cleric",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     isConnected = true;
